0685fbf5e3b631cbf7fe6602fc6049e924ec8936
shivkumargujarnew <shivkumargujar16@gmail.com>
2026-05-15 11:13:43 +0530

    final changes from main  devloper suresh

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -8,39 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class Controller {
 
-	  @GetMapping
-	  public String m1() {
-		  return "hello welcome to controller git practice";
-	  }
-	  
-	  @GetMapping
-	  public String m2() {
-		  return "hello welcome to controller m2 git practice welcome m22";
-	  }
-
-	  @GetMapping
-	  public String m3() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 2";
-	  }
-
-       @GetMapping
-	  public String m4() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1";
-	  }
-	    @GetMapping
-	  public String m5() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 2 methiod m5 addede";
-	  }
-          @GetMapping
-	  public String m5() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1 methiod m6 addede";
-	  }
-	  @GetMapping
-	  public String m44() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1";
-	  }
-	  @GetMapping
-	  public String m47() {
-		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1";
-	  }
+	 public String hello(){
+		 return "hello";
 }
