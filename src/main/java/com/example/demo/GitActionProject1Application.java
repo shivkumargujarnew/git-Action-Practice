e05f35b6eb318a59cea898ad9cfdc847ce6ced58
shivkumar <shivkumargujar16@gmail.com>
2026-05-15 09:50:18 +0530

    first commit

---
+++ src/main/java/com/example/demo/GitActionProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionProject1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class Controller {
+
+	  @GetMapping
+	  public String m1() {
+		  return "hello welcome to controller git practice";
+	  }
+}
