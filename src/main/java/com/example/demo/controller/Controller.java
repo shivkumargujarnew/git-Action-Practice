9bdb1e5c3615ecd1a572b4fcbbebdde2fa3e6791
shivkumar <shivkumargujar16@gmail.com>
2026-05-15 10:29:22 +0530

    change in git controller m5

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -32,7 +32,7 @@ public class Controller {
 		  return "hello welcome to controller m2 git practice welcome m33 added devloper 2 methiod m5 addede";
 	  }
           @GetMapping
-	  public String m5() {
+	  public String m51() {
 		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1 methiod m6 addede";
 	  }
 }
