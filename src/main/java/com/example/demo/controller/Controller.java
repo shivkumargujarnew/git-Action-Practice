0cb3bc0fedc974843efd1bcd6595fd6ed5b176b2
shivkumargujarnew <shivkumargujar16@gmail.com>
2026-05-15 10:12:33 +0530

    devloper 2 method m5 added

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -27,4 +27,8 @@ public class Controller {
 	  public String m4() {
 		  return "hello welcome to controller m2 git practice welcome m33 added devloper 1";
 	  }
+	    @GetMapping
+	  public String m5() {
+		  return "hello welcome to controller m2 git practice welcome m33 added devloper 2 methiod m5 addede";
+	  }
 }
